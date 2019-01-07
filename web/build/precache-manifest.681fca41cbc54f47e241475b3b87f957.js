9e7a078d32cf328b1250c5068702935110c7dd32
SaNdRiZzZ <alejandrozapeta@gmail.com>
2019-01-06 20:57:15 -0600

    produccion

---
+++ web/build/precache-manifest.681fca41cbc54f47e241475b3b87f957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "44c5a2cafca413a50000",
+    "url": "/innovare-prueba/static/js/runtime~main.44c5a2ca.js"
+  },
+  {
+    "revision": "22e8bb414fcbff0e3992",
+    "url": "/innovare-prueba/static/js/main.22e8bb41.chunk.js"
+  },
+  {
+    "revision": "dafe983ce9e49c19708d",
+    "url": "/innovare-prueba/static/js/1.dafe983c.chunk.js"
+  },
+  {
+    "revision": "22e8bb414fcbff0e3992",
+    "url": "/innovare-prueba/static/css/main.c1f3f8fa.chunk.css"
+  },
+  {
+    "revision": "dafe983ce9e49c19708d",
+    "url": "/innovare-prueba/static/css/1.cf047e82.chunk.css"
+  },
+  {
+    "revision": "692a99ca162ff1c9e9b1abc52f5c2155",
+    "url": "/innovare-prueba/index.html"
+  }
+];
